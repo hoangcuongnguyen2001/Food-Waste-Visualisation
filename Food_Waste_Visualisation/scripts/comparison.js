fee8e1faf0797b34702b47f4faf007d6a36ccc9a
illonion <zhou.lawrence.yz@gmail.com>
2021-05-12 21:21:36 +1000

    Add graph to 3rd Visualisation with incomplete xAxis

--- Food_Waste_Visualisation/scripts/comparison.js
+++ Food_Waste_Visualisation/scripts/comparison.js
@@ -5,24 +5,8 @@ var third_width = 800;
 var third_height = 400;
 var third_padding = 30;
   	
-
-var third_data = [{Country:"Australia", Household_Food_Waste:102},
-	{Country:"Austria", Household_Food_Waste:39},
-	{Country:"Denmark", Household_Food_Waste:81},
-	{Country:"Netherlands", Household_Food_Waste:50},
-	{Country:"New Zealand", Household_Food_Waste:61},
-	{Country:"Norway", Household_Food_Waste:79},
-	{Country:"Sweden", Household_Food_Waste:81},
-	{Country:"United Kingdom", Household_Food_Waste:77},
-	{Country:"USA", Household_Food_Waste:59},
-	{Country:"Japan", Household_Food_Waste:64},
-	{Country:"Poland", Household_Food_Waste:56},
-	{Country:"Finland", Household_Food_Waste:65},
-	{Country:"Italy", Household_Food_Waste:67},
-	{Country:"France", Household_Food_Waste:85},
-    {Country:"Belgium", Household_Food_Waste:50}];
-
-
+var third_countries = ["Australia", "Austria", "Denmark", "Netherlands","New Zealand", "Norway", "Sweden", "United Kingdom","USA","Japan","Poland","Finland","Italy","France","Belgium"]
+var third_data = [102, 39, 81, 50, 61, 79, 81, 77, 59, 64, 56, 65, 57, 85, 50];
 
 var third_xScale = d3.scaleBand()
 	                 .domain(d3.range(third_data.length))
@@ -30,13 +14,11 @@ var third_xScale = d3.scaleBand()
 	                 .paddingInner(0.1);
 
 var third_yScale = d3.scaleLinear()
-                     .domain([0,				
-                       d3.max(third_data, function(d) {
-                           return d.Household_Food_Waste;
-                        })
-                      ])
+                     .domain([0, d3.max(third_data)])
 	                 .range([third_height - third_padding, third_padding]);
 
+var third_xAxis = d3.axisBottom()
+					.scale(third_xScale).tickFormat(function(i) {return third_countries[i];})
 
 var third_yAxis = d3.axisLeft()
                     .scale(third_yScale).ticks(5);
@@ -45,8 +27,6 @@ var third_svg = d3.select("#chart3")
                   .append("svg")
                   .attr("width", third_width)
                   .attr("height", third_height);
-
-
 
 third_svg.selectAll("rect")
 	.data(third_data)
@@ -75,15 +55,12 @@ third_svg.append('text')
         .style('font-size', 'small')
         .text('Food waste per capita (kg/year)');
 
-
-
-
 third_svg.append("g")
          .attr("class", "axis")
          .attr("transform", "translate(" + third_padding + ",0)")
          .call(third_yAxis);
-      
 
-
-
-
+third_svg.append("g")
+         .attr("class", "axis")
+         .attr("transform", "translate(" + (third_height - third_padding) + ",0)")
+         .call(third_xAxis);
